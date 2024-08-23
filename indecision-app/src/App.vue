d35f3ba28a3f90f833df6ea02ae6925167cb512f
AlexxByron <alexbyron23@gmail.com>
2024-08-23 14:45:45 -0600

    Copia

---
+++ indecision-app/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>My App</h1>
+  <hr />
+
+  <MyCounter :value="7" />
+  <MyCounterScript :value="7" />
+</template>
+
+<script lang="ts" setup>
+import MyCounter from './components/MyCounter.vue'
+import MyCounterScript from './components/MyCounterScript.vue'
+</script>
---
+++ indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h3>Contador: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn btn-primary" @click="counter++">+1</button>
+      <button class="btn btn-primary" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+
+interface Props {
+  value: number
+}
+
+const props = defineProps<Props>()
+
+const counter = ref(props.value)
+const squareCounter = computed(() => counter.value * counter.value)
+</script>
