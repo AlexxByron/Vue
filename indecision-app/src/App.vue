c9456bff6ffee8c89ffe38577d34c6860005c4f2
Alexander Byron <alexbyron23@gmail.com>
2024-08-26 17:51:30 -0600

    copia2

--- indecision-app/src/App.vue
+++ indecision-app/src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <h1>My App</h1>
-  <hr />
-
-  <MyCounter :value="7" />
-  <MyCounterScript :value="7" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue'
-import MyCounterScript from './components/MyCounterScript.vue'
+import IndecisionView from './views/IndecisionView.vue';
+
 </script>
---
+++ indecision-app/src/components/chat/ChatMensajes.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex-1 overflow-y-auto p-4">
+        <div class="flex flex-col space-y-2">
+<!-- Escribe Mensaje Aqui -->
+    <ChatBurbuja
+        v-for="message in messages" :key="message.id" v-bind="message"
+        />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { ChatMessage } from '@/interfaces/chat-message.interface'
+import ChatBurbuja from './ChatBurbuja.vue';
+
+
+interface Props {
+    messages: ChatMessage[];
+}
+defineProps<Props>();
+</script>
